baad732d67a1f7b05e88be073bc0e1b7b511b6db
Benjamin E Deckey <benjaminedeckey@Benjamins-MacBook-Pro.local>
2020-06-09 13:14:23 -0700

    added image to dhruv

--- _site/assets/js/index.js
+++ _site/assets/js/index.js
@@ -1,18 +1,28 @@
 new WOW().init();
 
-words = ['', 'independent','disruptors', 'confident', 'progressive', 'inclusive', 'tolerant',
-	'entrepreneurs', 'activists', 'leaders', 'innovators', 'world changers', 'Vivek', 'Ben', 'independent','disruptors', 'confident', 'progressive', 'inclusive', 'tolerant',
-	'entrepreneurs', 'activists', 'leaders', 'innovators', 'world changers',];
+words = ['independent','disruptors', 'confident', 'progressive', 'inclusive', 'tolerant',
+	'entrepreneurs', 'activists', 'leaders', 'innovators', 'world changers'];
 
 var i = 0;
+let rand = 0;
+let w = 0;
 var changeText = setInterval(() => {
 		i = i % (words.length - 1);
+		rand = Math.floor(Math.random() * 1000);
+		console.log(rand);
+		if (rand === 500) {
+			w = 'ben.'
+		} else if (rand === 501) {
+			w = 'vivek.'
+		} else {
+			w = words[i]+"."
+		}
 		$("#textchanging").fadeOut("fast", () => {
-			$("#textchanging").text(words[i]+".");
+			$("#textchanging").text(w);
 		});
 		$( "#textchanging").fadeIn("slow");
 		i++;
-}, 1500)
+}, 1500);
 
 $(".more").toggle(function(){
 	$(this).text("less..").siblings(".complete").show();
